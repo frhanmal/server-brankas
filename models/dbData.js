23ecea99c252e0e2cad91746d3ead571c7300f9a
frhanmal <farhanakmal268@gmail.com>
2024-04-19 15:31:53 +0700

    perubahan dekripsi

--- models/dbData.js
+++ models/dbData.js
@@ -2,14 +2,7 @@ const mongoose = require("mongoose");
 
 const dataSchema = new mongoose.Schema(
   {
-    status_pintu: String,
-    teks_asli: String,
     teks_enkripsi: String,
-    teks_dekripsi: String,
-    pin_lama: String,
-    teks_enkripsi_sebelum: String,
-    pin_baru: String,
-    teks_enkripsi_sesudah: String,
   },
   {
     timestamps: true,
--- routes/dataRoute.js
+++ routes/dataRoute.js
@@ -19,27 +19,13 @@ function decrypt(encryptedBase64Text, key, iv) {
 
 router.post("/kirimData", async (req, res) => {
   const {
-    status_pintu,
-    teks_asli,
     teks_enkripsi,
-    teks_dekripsi,
-    pinLama,
-    teks_enkripsi_sebelum,
-    pinBaru,
-    teks_enkripsi_sesudah,
   } = req.body;
 
   const timestamp = new Date();
 
   const newData = new Data({
-    status_pintu: status_pintu,
-    teks_asli: teks_asli,
     teks_enkripsi: teks_enkripsi,
-    teks_dekripsi: teks_dekripsi,
-    pin_lama: pinLama,
-    teks_enkripsi_sebelum: teks_enkripsi_sebelum,
-    pin_baru: pinBaru,
-    teks_enkripsi_sesudah: teks_enkripsi_sesudah,
   });
 
   try {
@@ -49,8 +35,10 @@ router.post("/kirimData", async (req, res) => {
   } catch (err) {
     console.log("Gagal menyimpan data semua:", err);
     res.status(500).json({ message: "Gagal menyimpan data semua" });
-  }
+  } 
 });
+
+
 
 router.get("/getDataAll", async (req, res) => {
   try {
